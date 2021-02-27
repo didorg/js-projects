cd8cf60faeeefe16ae7d6025e80913f26a5c5603
Didier <didorg@gmail.com>
2021-02-27 13:13:35 -0500

    Making a Forecast Class

--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -3,6 +3,7 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
 
 const updateUI = (data) => {
   // destructure properties
@@ -21,43 +22,36 @@ const updateUI = (data) => {
   // update the night/day & icon images
   const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
   icon.setAttribute('src', iconSrc);
-
+  
   const timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
   time.setAttribute('src', timeSrc);
 
   // remove the d-none class if present
-  if (card.classList.contains('d-none')) {
+  if(card.classList.contains('d-none')){
     card.classList.remove('d-none');
   }
-};
-
-const updateCity = async (city) => {
-
-  const cityDets = await getCity(city);
-  const weather = await getWeather(cityDets.Key);
-  return { cityDets, weather };
-
 };
 
 cityForm.addEventListener('submit', e => {
   // prevent default action
   e.preventDefault();
-
+  
   // get city value
   const city = cityForm.city.value.trim();
   cityForm.reset();
 
   // update the ui with new city
-  updateCity(city)
+  forecast.updateCity(city)
     .then(data => updateUI(data))
     .catch(err => console.log(err));
 
   // set local storage
   localStorage.setItem('city', city);
+
 });
 
 if(localStorage.getItem('city')){
-  updateCity(localStorage.getItem('city'))
+  forecast.updateCity(localStorage.getItem('city'))
     .then(data => updateUI(data))
     .catch(err => console.log(err));
 }
